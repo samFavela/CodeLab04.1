2d6de79e74cbb6d479e3aa3d286e93d82afe1351
samFavela <MinoCino2!>
2020-02-11 11:18:51 -0600

    initial commit

--- app/src/main/java/com/example/droidcafedemo/OrderActivity.java
+++ app/src/main/java/com/example/droidcafedemo/OrderActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
-    TextView order_textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
